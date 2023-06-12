d9aa5b7f92c7e58f35d84ebe33b38c483693afa7
ItzDoctorWho <essouaiedmedhelmi@gmail.com>
2023-06-12 19:14:33 +0100

    pages folder added

--- lib/widgets/event_card.dart
+++ lib/widgets/event_card.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:issatso_events/eventpage.dart';
+import 'package:issatso_events/pages/eventpage.dart';
 
 class EventCard extends StatefulWidget {
   const EventCard({Key? key}) : super(key: key);
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:issatso_events/main.dart';
+import 'package:issatso_events/pages/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
